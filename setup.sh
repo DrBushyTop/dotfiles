05c2ad3720711de242a29424e97f6e1a4114a57a
Pasi Huuhka <aakkoset@gmail.com>
2020-09-27 16:06:48 +0300

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -46,7 +46,7 @@ then
 
     cp -f ~/dotfiles/.zshrc ~/.zshrc
     chsh -s /usr/bin/zsh $USERNAME
-    wget https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh -O - | zsh
+    sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
     git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
     git clone https://github.com/zsh-users/zsh-syntax-highlighting.git
     echo "source $PWD/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh" >> ${ZDOTDIR:-$HOME}/.zshrc
