cbedd04c8b5eccd666c5f59d211a59253cce74f1
DrBushyTop <aakkoset@gmail.com>
2021-05-13 22:43:16 +0300

    plugin updates

--- .config/zsh/nnn.zsh
+++ .config/zsh/nnn.zsh
@@ -1,6 +1,10 @@
 # NNN
 export NNN_PLUG='o:fzopen;c:fcd;j:autojumpfasd'
 export NNN_FIFO="/tmp/nnn.fifo"
-
 export NNN_FCOLORS='0000E63100000000000000000'
-alias nnn="~/.config/nnn/nnn -e"
+export NNN_TMPFILE='true'
+export NNN_USE_EDITOR=1
+
+source ~/.config/zsh/quitcd.zsh
+
+alias nnn="n -e"
