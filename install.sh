05da70b7bd1196ea91a605c171024c0995b92084
Pasi Huuhka <pasi.huuhka@zure.com>
2023-04-03 21:42:06 +0300

    add missing

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+
 brew install stow
-brew install wget
 brew install git
 
 stow .
 
 # Configure Zsh
-sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended --keep-zshrc
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-$HOME/}zsh-syntax-highlighting
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
@@ -19,3 +20,8 @@ git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$
 # - Chrome
 # - Arc
 # - Goland / Jetbrains
+# - Kitty
+# - LiceCap
+# - Postman
+# - Obsidian
+# - Presentify
