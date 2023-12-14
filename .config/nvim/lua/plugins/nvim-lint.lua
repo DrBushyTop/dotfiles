2a1ca56af4fc8e880ea36c2eec2b816266c7eaeb
Pasi Huuhka <pasi.huuhka@zure.com>
2023-12-14 11:22:28 +0200

    conform?

---
+++ .config/nvim/lua/plugins/nvim-lint.lua
@@ -0,0 +1,15 @@
+return {
+  "mfussenegger/nvim-lint",
+  opts = {
+    linters_by_ft = {
+      go = { "golangcilint" },
+      -- lua = { "luacheck" },
+      markdown = { "markdownlint" },
+      json = { "jsonlint" },
+      yaml = { "yamllint" },
+      zsh = { "zsh" },
+      html = { "tidy" },
+      templ = { "tidy" },
+    },
+  },
+}
